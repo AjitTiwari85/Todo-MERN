03693f61dfdd24d1f7868f972a8e466bea21fa1c
AjitTiwari85 <at856575@gmail.com>
2025-12-15 00:08:56 +0530

    add task routes and controller for task management

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,15 @@
-import express from 'express';
-
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World from Express.js");
 });
-
-
-
 
 export default app;
