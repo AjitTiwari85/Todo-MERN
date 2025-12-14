2bd5b426993f893588be21ab3e214a25cbedb4be
AjitTiwari85 <at856575@gmail.com>
2025-12-14 23:43:58 +0530

    refactor server setup and add MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
-import app from './src/app.js';
+import dotenv from "dotenv";
+import app from "./src/app.js";
 
+import connectDB from "./src/config/db.js";
 
-app.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000");
-});  
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
